3fbc6e8348b57fde0dd9a7538ef500fecbbd37d2
Sanghyuk Lee <givemesomefoodplz@gmail.com>
2023-05-01 14:05:02 +0900

    feat: 입력값에서 Category 타입별 구현 클래스 객체 생성 후 저장소에 저장하는 기능 구현

--- src/main/java/week2/Kiosk/service/KioskService.java
+++ src/main/java/week2/Kiosk/service/KioskService.java
@@ -1,11 +1,40 @@
 package week2.Kiosk.service;
 
+import week2.Kiosk.Util.Validator;
+import week2.Kiosk.domain.*;
 import week2.Kiosk.repository.KioskRepository;
+
+import static week2.Kiosk.domain.Category.*;
 
 public class KioskService {
     private final KioskRepository kioskRepository;
 
     public KioskService(KioskRepository kioskRepository) {
         this.kioskRepository = kioskRepository;
+    }
+
+    public void uploadProduct(String info) {
+        Validator.validateUpload(info);
+
+        String[] infoArr = info.split(",");
+
+        Item item = null;
+
+        switch (Category.from(infoArr[0])) {
+            case BAG:
+                item = new Bag(infoArr[1], Integer.parseInt(infoArr[2]));
+                break;
+            case FOOD:
+                item = new Food(infoArr[1], Integer.parseInt(infoArr[2]));
+                break;
+            case CLOTHES:
+                item = new Clothes(infoArr[1], Integer.parseInt(infoArr[2]));
+                break;
+            case BEVERAGE:
+                item = new Beverage(infoArr[1], Integer.parseInt(infoArr[2]));
+                break;
+        }
+
+        kioskRepository.upload(Category.from(infoArr[0]), item);
     }
 }
